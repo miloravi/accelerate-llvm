ee38e357313bbd68c33546a5305433b32aa1650f
miloravi <57906122+miloravi@users.noreply.github.com>
2026-02-01 02:36:15 +0100

    re-added the ealier removed code to make folds work again

--- accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen.hs
+++ accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen.hs
@@ -1022,6 +1022,28 @@ parCodeGenScan descending foldOrScan fun seed input index codeSeed codePre codeP
             -- as this loop starts with the prefix value of the previous
             -- thread. We can directly write that to kernel memory.
             return local
+          else if isNothing seed then
+            -- If there is no seed, then write the output directly in the first tiles.
+            -- The other tiles must combine their result with the given operator.
+            -- Note that the first tile should typically be handled in the sequential mode,
+            -- but this sequential mode is not always generated:
+            -- A non-commutative fold is handled as a scan without the sequential mode.
+            -- Furthermore we could decide to skip the sequential mode if it leads to
+            -- a lot of code duplication (but we don't do that yet).s
+            A.ifThenElse (tp, A.eq singleType (envsTileIndex envs) (A.liftInt 0))
+              (do
+                return local
+              )
+              (do
+                prefix <- tupleLoad tp valuePtrs
+                tupleStore tp accumVar prefix
+                if envsDescending envs then
+                  app2 (llvmOfFun2 (compileArrayInstrEnvs envs) fun) local prefix
+                else
+                  app2 (llvmOfFun2 (compileArrayInstrEnvs envs) fun) prefix local
+              )
+          -- If there is a seed, then all tile will combine their local result with
+          -- the already available value.
           else do
             -- If there is no seed, then write the output directly in the first tiles.
             -- The other tiles must combine their result with the given operator.
